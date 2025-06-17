46b61a8bce4a91fb8c9af18a37237a2431be4461
saravanan-smartowner <saravanan.subramanian@smartowner.com>
2025-06-17 14:53:37 +0530

    Update Vendor.php

--- it-asset-management/app/Models/Vendor.php
+++ it-asset-management/app/Models/Vendor.php
@@ -1,27 +1,47 @@
 <?php
 
 namespace App\Models;
-use App\Models\SoftwareLicense;
-use App\Models\Purchase;
-use App\Models\Asset;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Vendor extends Model
 {
-    //
-public function assets()
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'contact_person',
+        'phone',
+        'email',
+        'products_services_supplied',
+        'sla_terms',
+        'escalation_matrix',
+    ];
+
+    // Relationships
+    public function assets()
     {
+        // A vendor can be directly linked to assets if an asset has a 'vendor_id' for supplier info
         return $this->hasMany(Asset::class);
     }
 
     public function purchases()
     {
+        // A vendor can have many purchase orders
         return $this->hasMany(Purchase::class);
     }
 
     public function softwareLicenses()
     {
+        // A vendor can supply many software licenses
         return $this->hasMany(SoftwareLicense::class);
     }
+
+    // You might also have a relationship for Warranties if a vendor is an AMC provider
+    // public function warranties()
+    // {
+    //     return $this->hasMany(Warranty::class, 'amc_vendor_id'); // Assuming 'amc_vendor_id' on warranties table
+    // }
 }
